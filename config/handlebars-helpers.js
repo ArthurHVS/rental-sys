61f7e5d840f99503ba4a4b66bfc6cc26955fef02
Arthur <arthur.hvs@gmail.com>
2021-02-22 15:23:39 -0300

    Ta chegando a hora...

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -4,5 +4,11 @@ module.exports = {
         var m = new Date(born).getMonth()
         var d = new Date(born).getDate();
         return " " + d + "/" + m;
+    },
+    first_mf: function (num) {
+        if (num == 0) {
+            return true
+        }
+        else return false;
     }
 }
